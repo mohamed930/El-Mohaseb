7289fad3c6777d980232e59687ac287795079edd
Mohamed Ali <mohammedali12477@gmail.com>
2020-10-31 15:03:50 -0700

    Make Some View without Add view Commit

---
+++ ElMohaseb Soft/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElMohaseb Soft
+//
+//  Created by Mohamed Ali on 10/31/20.
+//  Copyright © 2020 Mohamed Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
