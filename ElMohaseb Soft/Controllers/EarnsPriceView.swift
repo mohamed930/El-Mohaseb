40598b98bf123b5376c8d10e0d3e6044a793a507
Mohamed Ali <mohammedali12477@gmail.com>
2020-12-06 16:23:15 -0800

    Add Some Files wasn't Commit

---
+++ ElMohaseb Soft/Controllers/EarnsPriceView.swift
@@ -0,0 +1,21 @@
+//
+//  EarnsPriceView.swift
+//  ElMohaseb Soft
+//
+//  Created by Mohamed Ali on 12/6/20.
+//  Copyright © 2020 Mohamed Ali. All rights reserved.
+//
+
+import UIKit
+
+class EarnsPriceView: UIView {
+
+    @IBOutlet weak var ProductName: UITextField!
+    @IBOutlet weak var ProductPrice: UITextField!
+    @IBOutlet weak var ProductNote: UITextField!
+    @IBOutlet weak var CalculatorImage: UIImageView!
+    @IBOutlet weak var Line1: UIView!
+    @IBOutlet weak var Line2: UIView!
+    @IBOutlet weak var Line3: UIView!
+
+}
